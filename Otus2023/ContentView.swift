fcc75819fea3f410b345b4756d0d51790bc73dc6
Dmitriy Obrazumov <obrazumovd@mail.ru>
2023-06-18 13:51:22 +0300

    Initial Commit

---
+++ Otus2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Otus2023
+//
+//  Created by Dmitriy Obrazumov on 18/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Otus2023/Otus2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus2023App.swift
+//  Otus2023
+//
+//  Created by Dmitriy Obrazumov on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Otus2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
